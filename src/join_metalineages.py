6482bf84906e81ea257bf956e54897c9525d3fe5
David <icestorm@gmx.de>
2022-05-21 18:46:07 +0200

    MyDocuments folder is wrong in windows, if the user changed its default path. Added bugfix based on stackexchange suggestion

--- src/join_metalineages.py
+++ src/join_metalineages.py
@@ -37,7 +37,18 @@ COLS = {
     }
 
 # Store in "Documents" folder. Should work in Windows & Ubuntu
-FPATH = os.path.expanduser(f'~{os.sep}Documents')
+if os.name == 'nt': # WINDOWS
+    # Src: https://stackoverflow.com/questions/6227590/finding-the-users-my-documents-path
+    import ctypes.wintypes
+    CSIDL_PERSONAL = 5       # My Documents
+    SHGFP_TYPE_CURRENT = 0   # Get current, not default value
+    
+    buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
+    ctypes.windll.shell32.SHGetFolderPathW(None, CSIDL_PERSONAL, None, SHGFP_TYPE_CURRENT, buf)
+    
+    FPATH = buf.value
+else:
+    FPATH = os.path.expanduser(f'~{os.sep}Documents')
 FNAME_OUT = f'{FPATH}{os.sep}meta_lineages_{DATE}.csv'
 
 # Download and read CSV files from RKI Github
